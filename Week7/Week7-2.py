e2cbb7091154a5647abd55addf02c623f494ccdf
lorivo <77751574+lorivo@users.noreply.github.com>
2021-07-28 13:04:19 -0500

    Add files via upload

--- Week7/Week7-2.py
+++ Week7/Week7-2.py
@@ -3,14 +3,19 @@ import numpy as np
 import pickle as pkl
 import matplotlib.pyplot as plt
 from scipy import signal
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-from sklearn import preprocessing, tree
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+#testing if code works
 
 data = pd.read_csv('finaldata1.csv')
 data.columns = ['timestamp','acc_x','acc_y','acc_z','orientation','label']
@@ -21,84 +26,86 @@ datascroll = data.loc[data['label'] == 1]
 acc_xs = np.array(datascroll['acc_x'])
 plt.xlabel('time (sec)')
 plt.ylabel("acc_x scroll (m/s^2)")
-plt.plot(acc_xs)
-plt.title('Accelerometer X-Axis Scroll Application')
 plt.ylim([-10,35])
+plt.plot(acc_xs)
+plt.title('x axis scroll application')
 plt.show()
 
 acc_xyt = np.array(datayt['acc_x'])
 plt.xlabel('time (sec)')
-plt.ylabel("acc_x video (m/s^2)")
-plt.plot(acc_xyt)
-plt.title('Accelerometer X-Axis Video Application')
+plt.ylabel("acc_x youtube (m/s^2)")
 plt.ylim([-10,35])
+plt.plot(acc_xyt)
+plt.title('x axis youtube application')
 plt.show()
 
 acc_ys = np.array(datascroll['acc_y'])
 plt.xlabel('time (sec)')
 plt.ylabel("acc_y scroll (m/s^2)")
-plt.plot(acc_ys)
-plt.title('Accelerometer Y-Axis Scroll Application')
 plt.ylim([-10,35])
+plt.plot(acc_ys)
+plt.title('y axis scroll application')
 plt.show()
 
 acc_yyt = np.array(datayt['acc_y'])
 plt.xlabel('time (sec)')
-plt.ylabel("acc_y video (m/s^2)")
-plt.plot(acc_yyt)
-plt.title('Accelerometer Y-Axis Video Application')
+plt.ylabel("acc_y youtube (m/s^2)")
 plt.ylim([-10,35])
+plt.plot(acc_yyt)
+plt.title('y axis youtube application')
 plt.show()
 
 acc_zs = np.array(datascroll['acc_z'])
 plt.xlabel('time (sec)')
 plt.ylabel("acc_z scroll (m/s^2)")
-plt.plot(acc_zs)
-plt.title('Accelerometer Z-Axis Scroll Application')
 plt.ylim([-10,35])
+plt.plot(acc_zs)
+plt.title('z axis scroll application')
 plt.show()
 
 acc_zyt = np.array(datayt['acc_z'])
 plt.xlabel('time (sec)')
-plt.ylabel("acc_z video (m/s^2)")
-plt.plot(acc_zyt)
-plt.title('Accelerometer Z-Axis Video Application')
+plt.ylabel("acc_z youtube (m/s^2)")
 plt.ylim([-10,35])
+plt.plot(acc_zyt)
+plt.title('z axis youtube application')
 plt.show()
 
 X = np.array(data[['acc_x','acc_y','acc_z','orientation']])
 y = np.array(data[['label']])
-
 scaler = StandardScaler()
 # transform data
 scaled = scaler.fit_transform(X)
 #saving the scaler for future use
 pkl.dump(scaler,open('scaler.pkl','wb'))
-
 from sklearn.model_selection import KFold
 trainscores= []
 testscores = []
 gnb = GaussianNB()
-cv = KFold(n_splits=5, random_state=42, shuffle=True)
+cv = KFold(n_splits=5, random_state=42, shuffle=False)
 for train_index, test_index in cv.split(scaled):
+    #print("Train Index: ", train_index, "\n")
+    #print("Test Index: ", test_index)
 
     X_train, X_test, y_train, y_test = scaled[train_index], scaled[test_index], y[train_index], y[test_index]
     gnb.fit(X_train, y_train)
     trainscores.append(gnb.score(X_train,y_train))
     testscores.append(gnb.score(X_test, y_test))
+    import statistics
 
-import statistics
-gnbtrain = statistics.mean(trainscores)
-gnbtest = statistics.mean(testscores)
-print("Gaussian Naive Bayes Mean Training Accuracy:",gnbtrain )
-print("Gaussian Naive Bayes Mean Testing Accuracy:", gnbtest )
-pkl.dump(gnb,open('gnb.pkl','wb'))
+    gnbtrain = statistics.mean(trainscores)
+    gnbtest = statistics.mean(testscores)
+    print("Mean Training accuracy", gnbtrain)
+    print("Mean Testing Accuracy", gnbtest)
+    pkl.dump(gnb, open('gnb.pkl', 'wb'))
 
 trainscores= []
 testscores = []
 svm = SVC(kernel='linear')
-cv = KFold(n_splits=5, random_state=42, shuffle=True)
+cv = KFold(n_splits=5, random_state=42, shuffle=False)
 for train_index, test_index in cv.split(scaled):
+    #print("Train Index: ", train_index, "\n")
+    #print("Test Index: ", test_index)
 
     X_train, X_test, y_train, y_test = scaled[train_index], scaled[test_index], y[train_index], y[test_index]
     svm.fit(X_train, y_train)
@@ -107,15 +114,16 @@ for train_index, test_index in cv.split(scaled):
 
 svmtrain = statistics.mean(trainscores)
 svmtest = statistics.mean(testscores)
-print("Support Vector Machine Mean Training Accuracy:",svmtrain )
-print("Support Vector Machine Mean Testing Accuracy:", svmtest )
+print("Mean Training accuracy",svmtrain )
+print("Mean Testing Accuracy", svmtest )
 pkl.dump(svm,open('svm.pkl','wb'))
-
 trainscores= []
 testscores = []
 destree = tree.DecisionTreeClassifier()
-cv = KFold(n_splits=5, random_state=42, shuffle=True)
+cv = KFold(n_splits=5, random_state=42, shuffle=False)
 for train_index, test_index in cv.split(scaled):
+    #print("Train Index: ", train_index, "\n")
+    #print("Test Index: ", test_index)
 
     X_train, X_test, y_train, y_test = scaled[train_index], scaled[test_index], y[train_index], y[test_index]
     destree.fit(X_train, y_train)
@@ -124,15 +132,18 @@ for train_index, test_index in cv.split(scaled):
 
 destrain = statistics.mean(trainscores)
 destest = statistics.mean(testscores)
-print("Decision Tree Mean Training Accuracy:",destrain)
-print("Decision Tree Mean Testing Accuracy:", destest)
+print("Mean Training accuracy",destrain )
+print("Mean Testing Accuracy", destest)
 pkl.dump(destree,open('destree.pkl','wb'))
+
 
 trainscores= []
 testscores = []
 randtree = RandomForestClassifier(n_estimators=100)
-cv = KFold(n_splits=5, random_state=42, shuffle=True)
+cv = KFold(n_splits=5, random_state=42, shuffle=False)
 for train_index, test_index in cv.split(scaled):
+    #print("Train Index: ", train_index, "\n")
+    #print("Test Index: ", test_index)
 
     X_train, X_test, y_train, y_test = scaled[train_index], scaled[test_index], y[train_index], y[test_index]
     randtree.fit(X_train, y_train)
@@ -141,10 +152,9 @@ for train_index, test_index in cv.split(scaled):
 
 rantrain = statistics.mean(trainscores)
 rantest = statistics.mean(testscores)
-print("Random Tree Mean Training Accuracy:",rantrain)
-print("Random Tree Mean Testing Accuracy:", rantest)
+print("Mean Training accuracy",rantrain )
+print("Mean Testing Accuracy", rantest )
 pkl.dump(randtree,open('randtree.pkl','wb'))
-
 trainacc = [gnbtrain, svmtrain, destrain, rantrain]
 testacc = [gnbtest, svmtest, destest, rantest]
 
@@ -153,7 +163,7 @@ width = 0.2
 
 # plot data in grouped manner of bar type
 plt.bar(x, trainacc, width, color='red')
-plt.bar(x + 0.2, testacc, width, color='green')
+plt.bar(x+0.2, testacc, width, color='green')
 plt.xticks(x, ['GNB', 'SVM', 'Descision Tree', 'Random Forest'])
 plt.xlabel("ML Models")
 plt.ylabel("Accuracies")
